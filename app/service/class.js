a59ddac6a55cbbf05b456fe3e18684ff7d1ad26f
pengyaowei <pengyaowei886@163.com>
2019-07-04 17:30:39 +0800

    d

--- app/service/class.js
+++ app/service/class.js
@@ -5,23 +5,15 @@ class ClassService extends Service {
     //查询分类列表
     async query_class(kind) {
         const mysql = this.app.mysql;
-        if (kind == 1) {
-            let result = await mysql.select('class', { where: { status: 1 }, columns: ['id', 'name'] });  
-            if (result.length >= 1) {
-                return result;
-            } else {
-                throw new Error("空数据");
-            }   
-        }else{
-            let result = await mysql.select('class', { where: { status: 1 }, columns: ['id', 'name'] });
-            if (result.length >= 1) {
-                return result;
-            } else {
-                throw new Error("空数据");
-            }   
-        }  
-        
-     
+        let result = await mysql.select('class', { where: { status: 1, kind: 1 }, columns: ['id', 'name'] });
+        if (result.length >= 1) {
+            return result;
+        } else {
+            throw new Error("空数据");
+        }
+
+
+
     }
 }
 module.exports = ClassService;
