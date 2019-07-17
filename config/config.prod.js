0ea098c5f1d6ba7e83b531cd0604752511598e01
pengyaowei <pengyaowei886@163.com>
2019-07-17 16:48:05 +0800

    djsmsan

--- config/config.prod.js
+++ config/config.prod.js
@@ -97,7 +97,7 @@ module.exports = appInfo => {
       };
     config.cluster = {
         listen: {
-            port: 8443,
+            port: 80,
             hostname: '172.30.0.2',
             https: {
                 key: "C:/soft/workface/https/https.key",
