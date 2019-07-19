a1f30f4e53321e43b05ac51f64e1740a7007185c
pengyaowei <pengyaowei886@163.com>
2019-07-19 18:01:16 +0800

    fiukjgsad

--- app/service/trolley.js
+++ app/service/trolley.js
@@ -9,23 +9,22 @@ class TrolleyService extends Service {
         const mysql =this.app.mysql;
 
         let result = await redis.hgetall(`trolley:${uid}`);
-        console.log(result);
         let goods_id = [];
         let spec_id = [];
-        let num=[]
         for (let i in result) {
             let a = i.split(":")
             goods_id.push(a[0]);
             spec_id.push(a[1]);
         }
-        let spec_info = await mysql.select('specs', { where: { id: spec_id }, columns: ['id','cost_price', 'goods_id', 'spec'] });
+        console.log(result)
+        let spec_info = await mysql.select('specs', { where: { id: spec_id }, columns: ['id','sell_price', 'goods_id', 'spec'] });
         let goods_info = await mysql.select('goods', { where: { id: goods_id }, columns: ['id','introduce', 'head_pic','status'] });
         for (let i in goods_info) {
             if (goods_info[i].status == 1) {
                 for(let j in spec_info){
                     if(goods_info[i].id==spec_info[j].goods_id){
                         goods_info[i].spec_name=spec_info[j].spec;
-                        goods_info[i].cost_price=spec_info[j].cost_price;
+                        goods_info[i].sell_price=spec_info[j].sell_price;
                        let num =`${goods_info[i].id}:${spec_info[j].id}`;
                        goods_info[i].num=result[`${num}`];
                         if(spec_info[j].status!=1 ||spec_info[j].repertory<=0){
@@ -42,7 +41,7 @@ class TrolleyService extends Service {
                 for(let j in spec_info){
                     if(goods_info[i].id==spec_info[j].goods_id){
                         goods_info[i].spec_name=spec_info[j].spec;
-                        goods_info[i].cost_price=spec_info[j].cost_price
+                        goods_info[i].sell_price=spec_info[j].sell_price
                         goods_info[i].fujia="该商品已下架";
                         let num =`${goods_info[i].id}:${spec_info[j].id}`;
                         goods_info[i].num=result[`${num}`];
